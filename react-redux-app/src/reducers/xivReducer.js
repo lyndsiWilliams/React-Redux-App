8985552ae1c36da5c4d8024373f6726c487c0f31
lyndsiWilliams <kcatgirl@gmail.com>
2020-01-01 13:36:59 -0600

    Added songReducer to xivReducer.js

--- react-redux-app/src/reducers/xivReducer.js
+++ react-redux-app/src/reducers/xivReducer.js
@@ -1,16 +1,25 @@
 import {
   FETCH_MEMBERS_START,
   FETCH_MEMBERS_SUCCESS,
-  FETCH_MEMBERS_FAILURE
+  FETCH_MEMBERS_FAILURE,
+  FETCH_SONGS_START,
+  FETCH_SONGS_SUCCESS,
+  FETCH_SONGS_FAILURE
 } from '../actions/xivActions';
 
-const initialState = {
+const initialMemberState = {
   freeCompanyMembers: [],
   error: "",
   isFetching: false
 };
 
-function xivReducer(state=initialState, action) {
+const initialSongState = {
+  songs: [],
+  error: "",
+  isFetching: false
+};
+
+function xivReducer(state=initialMemberState, action) {
   console.log(state);
   switch (action.type) {
     case FETCH_MEMBERS_START:
@@ -27,6 +36,33 @@ function xivReducer(state=initialState, action) {
         freeCompanyMembers: action.payload
       };
     case FETCH_MEMBERS_FAILURE:
+      return {
+        ...state,
+        error: action.payload,
+        isFetching: false
+      };
+    default:
+      return state;
+  }
+}
+
+function songReducer(state=initialSongState, action) {
+  console.log(state);
+  switch (action.type) {
+    case FETCH_SONGS_START:
+      return {
+        ...state,
+        error: "",
+        isFetching: true
+      };
+    case FETCH_SONGS_SUCCESS:
+      return {
+        ...state,
+        error: "",
+        isFetching: false,
+        songs: action.payload
+      };
+    case FETCH_SONGS_FAILURE:
       return {
         ...state,
         error: action.payload,
