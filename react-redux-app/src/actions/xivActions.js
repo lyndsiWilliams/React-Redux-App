7475b529e9d996e16ed1c350526bb67c4c8e8cdc
lyndsiWilliams <kcatgirl@gmail.com>
2020-01-01 13:43:19 -0600

    Added getSongs to xivActions.js

--- react-redux-app/src/actions/xivActions.js
+++ react-redux-app/src/actions/xivActions.js
@@ -1,8 +1,14 @@
 import axios from 'axios';
 
+// Destructuring fetch command names
 export const FETCH_MEMBERS_START = "FETCH_MEMBERS_START";
 export const FETCH_MEMBERS_SUCCESS = "FETCH_MEMBERS_SUCCESS";
 export const FETCH_MEMBERS_FAILURE = "FETCH_MEMBERS_FAILURE";
+
+export const FETCH_SONGS_START = "FETCH_SONGS_START";
+export const FETCH_SONGS_SUCCESS = "FETCH_SONGS_SUCCESS";
+export const FETCH_SONGS_FAILURE = "FETCH_SONGS_FAILURE";
+
 
 export const getMembers = () => dispatch => {
   dispatch({ type: FETCH_MEMBERS_START });
@@ -15,6 +21,22 @@ export const getMembers = () => dispatch => {
       console.log(err.response);
       dispatch({
         type: FETCH_MEMBERS_FAILURE,
+        payload: `${err.response.status} ${err.response.data}`
+      });
+    });
+};
+
+export const getSongs = () => dispatch => {
+  dispatch({ type: FETCH_SONGS_START });
+  axios.get("https://xivapi.com/Orchestrion")
+    .then(res => {
+      console.log(res);
+      dispatch({ type: FETCH_SONGS_SUCCESS, payload: res.data.Results });
+    })
+    .catch(err => {
+      console.log(err.response);
+      dispatch({
+        type: FETCH_SONGS_FAILURE,
         payload: `${err.response.status} ${err.response.data}`
       });
     });
